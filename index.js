c17ea4039ac119b772c0f8523ae347c70c01bf42
Kat Gilbert <katgilbert99@gmail.com>
2021-12-11 12:26:24 -0500

    all working now

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 
 const data = require("./data");
-const sum = require("/.sum");
+const sum = require("./sum");
+const arrayShuffle = require("./node_modules/lodash/_arrayShuffle");
+const numberArray = require("./data");
+
+
 
 console.log(data.name);
 console.log(data.hometown);
 console.log(sum.sumOfArray(data.numberArray))
+
+//from lodash
+console.log(arrayShuffle(data.numberArray));
+
